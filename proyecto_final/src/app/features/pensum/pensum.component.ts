4efe0367bdff42b2b001e998d35f3a411e18916f
X-Jefferson-2005-X <vg24028@ues.edu.sv>
2026-06-15 16:17:37 -0600

    Creacion de la carpeta pensum, la cual se encargara de mostrar los pensum

---
+++ proyecto_final/src/app/features/pensum/pensum.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensum',
+  standalone: true,
+  imports: [],
+  templateUrl: './pensum.component.html',
+  styleUrl: './pensum.component.css'
+})
+export class PensumComponent {
+
+}
